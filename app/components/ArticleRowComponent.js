93b87e58edd3834986d5404909246b7392484ba1
Ozgur GUL <lugrugzo@gmail.com>
2017-10-16 17:02:06 +0300

    ArticleRowComponent

---
+++ app/components/ArticleRowComponent.js
@@ -0,0 +1,20 @@
+import React  from 'react'
+import { View, Text } from 'react-native'
+
+export default class ArticleRowComponent extends React.Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+
+        return (
+            <View>
+                <Text>
+                    asd
+                </Text>
+            </View>
+        )
+    }
+}
--- app/pages/IndexPage.js
+++ app/pages/IndexPage.js
@@ -1,15 +1,37 @@
 import React  from 'react'
-import { View, Text } from 'react-native'
+import { FlatList, View } from 'react-native'
+import HurriyetService from '../service/HurriyetService'
+import ArticleRowComponent from '../components/ArticleRowComponent'
 
 export default class IndexPage extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            articles: []
+        }
+    }
+
+    componentDidMount() {
+        this.articles()
+    }
+
+    articles() {
+        HurriyetService
+            .articles()
+            .then(r => this.setState({ articles: r.data }))
+    }
 
     render() {
 
         return (
             <View>
-                <Text>
-                    asd
-                </Text>
+                <FlatList
+                    data={this.state.articles}
+                    keyExtractor={item => item.Id}
+                    renderItem={({ item }) => <ArticleRowComponent article={item} />}
+                />
             </View>
         )
     }
