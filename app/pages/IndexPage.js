2cc44fc55de416ba4220da926e66ed7f80373722
Ozgur GUL <lugrugzo@gmail.com>
2017-10-16 16:34:05 +0300

    test index page created

---
+++ app/pages/IndexPage.js
@@ -0,0 +1,16 @@
+import React  from 'react'
+import { View, Text } from 'react-native'
+
+export default class IndexPage extends React.Component {
+
+    render() {
+
+        return (
+            <View>
+                <Text>
+                    asd
+                </Text>
+            </View>
+        )
+    }
+}
